5579ff16829159cf6e71be910692b396bc8a653a
83197aditya <117979584+83197aditya@users.noreply.github.com>
2025-03-15 21:04:00 +0000

    Rest Project

---
+++ RestAssured_Project/src/test/java/com/LoginTest.java
@@ -0,0 +1,25 @@
+package com.api.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() {
+        RestAssured.baseURI = "https://demoqa.com";
+        RequestSpecification request = RestAssured.given();
+        
+        request.header("Content-Type", "application/json");
+        request.body("{\"userName\": \"viday1234\",\"password\": \"Viday1234@\"}");
+
+        Response response = request.post("/Account/v1/Authorized");
+
+        // Debugging output
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+        System.out.println("Response Headers: " + response.getHeaders().toString());
+    }
+}
